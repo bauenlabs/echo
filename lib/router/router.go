abad16456b7187fa56b8179eae55a1037be81cb2
Grant Campbell <grant@pulsr.com>
2016-12-14 20:41:59 -0600

    basic redis connection working

--- lib/router/router.go
+++ lib/router/router.go
@@ -1,2 +1,19 @@
 // DOCUMENTME
-package router
+package main
+
+import (
+   "gopkg.in/redis.v5"
+   "fmt"
+)
+
+func main() {
+    client := redis.NewClient(&redis.Options{
+        Addr:     "localhost:6379",
+        Password: "", // no password set
+        DB:       0,  // use default DB
+    })
+
+    pong, err := client.Ping().Result()
+    fmt.Println(pong, err)
+    // Output: PONG <nil>
+}
