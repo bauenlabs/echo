87c58b63bb3d978bb4c7d26534775f760f3016c3
Grant Campbell <grant@pulsr.com>
2016-12-14 21:22:47 -0600

    moved redis client outside of lookup function

--- lib/router/router.go
+++ lib/router/router.go
@@ -1,20 +1,21 @@
 // DOCUMENTME
-package main
+package router
 
 import (
    "gopkg.in/redis.v5"
    "fmt"
 )
 
+//open connection 
+var Client = redis.NewClient(&redis.Options{
+  Addr:     "localhost:6379",
+  Password: "", // no password set
+  DB:       0,  // use default DB
+})
+
 //look up a key in redis and return its value
 func lookup(hash string ) {
-    client := redis.NewClient(&redis.Options{
-        Addr:     "localhost:6379",
-        Password: "", // no password set
-        DB:       0,  // use default DB
-    })
-
-    value, err := client.Get(hash).Result()
+    value, err := Client.Get(hash).Result()
     fmt.Println(value, err)
     // Output: PONG <nil>
 }
