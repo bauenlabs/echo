678ca7d6da70a9f04b0b3da8496604dc7fd52c11
Patrick Coffey <patrickcoffey48@gmail.com>
2016-12-14 22:31:45 -0600

    Progress on server

--- lib/server/server.go
+++ lib/server/server.go
@@ -4,6 +4,7 @@ package server
 
 import (
 	"echo/lib/concat"
+	"fmt"
 	"gopkg.in/gin-gonic/gin.v1"
 	"net/http"
 )
@@ -13,29 +14,52 @@ func Serve(port string) {
 	// Create a new gin router.
 	router := gin.Default()
 
-	// Respond to / requests.
-	router.GET("/", handleRequest)
-	router.POST("/", handleRequest)
-	router.PUT("/", handleRequest)
-	router.DELETE("/", handleRequest)
-	router.PATCH("/", handleRequest)
-	router.HEAD("/", handleRequest)
-	router.OPTIONS("/", handleRequest)
-
 	// Respond to /* requests.
-	router.GET("/:path", handleRequest)
-	router.POST("/:path", handleRequest)
-	router.PUT("/:path", handleRequest)
-	router.DELETE("/:path", handleRequest)
-	router.PATCH("/:path", handleRequest)
-	router.HEAD("/:path", handleRequest)
-	router.OPTIONS("/:path", handleRequest)
+	router.GET("/*param", handleGET)
+	router.POST("/*param", handlePOST)
+	router.PUT("/*param", handlePUT)
+	router.DELETE("/*param", handleDELETE)
+	router.PATCH("/*param", handlePATCH)
+	router.HEAD("/*param", handleHEAD)
+	router.OPTIONS("/*param", handleOPTIONS)
 
 	// Start the server on the specified port.
 	router.Run(concat.Concat(":", port))
 }
 
-// handleRequest handles all requests and hands them off to the router package.
-func handleRequest(c *gin.Context) {
+// handleGET handles POST requests and passes them off to the router.
+func handleGET(c *gin.Context) {
+	fmt.Println(c.Param("param"))
+	fmt.Println(c.Request.URL.Query())
 	c.String(http.StatusOK, "Hello world!")
+}
+
+// handlePOST handles POST requests and passes them off to the router.
+func handlePOST(c *gin.Context) {
+
+}
+
+// handlePUT handles PUT requests and passes them off to the router.
+func handlePUT(c *gin.Context) {
+
+}
+
+// handleDELETE handles DELETE requests and passes them off to the router.
+func handleDELETE(c *gin.Context) {
+
+}
+
+// handlePATCH handles PATCH requests and passes them off to the router.
+func handlePATCH(c *gin.Context) {
+
+}
+
+// handleHEAD handles HEAD requests and passes them off to the router.
+func handleHEAD(c *gin.Context) {
+
+}
+
+// handleOPTIONS handles OPTIONS requests and passes them off to the router.
+func handleOPTIONS(c *gin.Context) {
+
 }
