75262b0acd51da3c2071b4d385e39a853f1662a8
Patrick Coffey <patrickcoffey48@gmail.com>
2016-12-19 20:30:41 -0600

    Add testing tools and basic test for cache init

---
+++ lib/cache/cache_test.go
@@ -0,0 +1,17 @@
+// Package that contains tests for the cache package.
+package cache_test
+
+import (
+	"github.com/stretchr/testify/assert"
+	"github.com/voiceis/echo/lib/cache"
+	"os"
+	"reflect"
+	"testing"
+)
+
+// Test the initialization function.
+func TestInit(t *testing.T) {
+	assert.Equal(t, cache.RedisPort, os.Getenv("ECHO_REDIS_PORT"))
+	assert.Equal(t, cache.RedisHost, os.Getenv("ECHO_REDIS_HOST"))
+	assert.Equal(t, reflect.TypeOf(cache.Client).String(), "*redis.Client")
+}
