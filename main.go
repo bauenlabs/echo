6dae639b94c737b5a8e4160887eb51867d92fa18
Patrick Coffey <patrickcoffey48@gmail.com>
2017-02-15 19:15:42 -0600

    Remove stack impact

--- main.go
+++ main.go
@@ -2,18 +2,10 @@
 package main
 
 import (
-	"github.com/stackimpact/stackimpact-go"
 	"github.com/voiceis/echo/lib/server"
 )
 
 // Executes the Echo server.
 func main() {
-	// Set up profiling.
-	agent := stackimpact.NewAgent()
-	agent.Start(stackimpact.Options{
-		AgentKey: "ea7aa282cb7ef7eba851796d7553a1bd7652bace",
-		AppName:  "Echo",
-	})
-
 	server.Serve()
 }
